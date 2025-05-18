9ad226d7e59f70e28b9fb362db7d48111c3b4cee
Exequiel Rojas <exequiel_rojasfernandez@hotmail.com>
2025-05-18 12:27:30 -0300

    Agregue las funciones para el formulario en javascript

--- js/app.js
+++ js/app.js
@@ -26,27 +26,35 @@ class Persona {
 
   mostrarGeneracion() {
     if (this.#anioNacimiento <= 1948) {
-        
+        alert(`${this.#nombre} es perteneciente a la Silent Generation y su rasgo caracteristico es: "Austeridad"`)
     } else if (this.#anioNacimiento <= 1968) {
-
+      alert(`${this.#nombre} es perteneciente a la Baby Boom y su rasgo caracteristico es: "Ambicion"`)
     } else if (this.#anioNacimiento <= 1980) {
-
+      alert(`${this.#nombre} es perteneciente a la Generacion X y su rasgo caracteristico es: "Obsesion por el exito"`)
     } else if (this.#anioNacimiento <= 1993) {
-
+      alert(`${this.#nombre} es perteneciente a la Generacion Y (millennials) y su rasgo caracteristico es: "Frustracion"`)
     } else if (this.#anioNacimiento <= 2010) {
-
+      alert(`${this.#nombre} es perteneciente a la Generacion Z y su rasgo caracteristico es: "Irreverencia"`)
     }
   }
 
   esMayorDeEdad() {
     if(this.#edad >= 18){
-
+      alert(`${this.#nombre} es mayor de edad`)
     } else {
-
+      alert(`${this.#nombre} es menor de edad`)
     }
   }
 
   mostrarDatos() {
-    
+    alert(`Datos del usuario:
+      Nombre: ${this.#nombre}
+      Edad: ${this.#edad}
+      DNI: ${this.#DNI}
+      Sexo: ${this.#sexo}
+      Peso: ${this.#peso}
+      Altura: ${this.#altura}
+      Año de Nacimiento: ${this.#anioNacimiento}
+      `)
   }
 }
